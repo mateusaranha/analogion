7daba6bdb539ded6bcd0c35c012c39bc7a721ef1
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-11 15:51:37 -0300

    Load repeat select layout fixes

--- main.tsx
+++ main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import Home from "./app/page";
 import "./app/globals.css";
 import "./app/repository-library.css";
+import "./app/select-controls.css";
 
 createRoot(document.getElementById("root")!).render(<Home />);
