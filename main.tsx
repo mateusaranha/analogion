f6abb09423a6866bca5a2bf38650734e3ac66667
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-11 18:10:44 -0300

    Mount Recolhimento mode controller

--- main.tsx
+++ main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Home from "./app/page";
+import { RecollectionMode } from "./components/recollection-mode";
 import "./app/globals.css";
 import "./app/repository-library.css";
 import "./app/select-controls.css";
 import "./app/listening-mode.css";
 
-createRoot(document.getElementById("root")!).render(<Home />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Home />
+    <RecollectionMode />
+  </>,
+);
