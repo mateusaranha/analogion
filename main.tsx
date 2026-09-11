ff2be0015f77e968f682cbba66c617df7bcc8819
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-11 18:13:19 -0300

    Add fullscreen Recolhimento mode (#22)
    * Add Recolhimento fullscreen controller
    
    * Style Recolhimento immersive mode
    
    * Mount Recolhimento mode controller
    
    * Load Recolhimento styles after app styles
    
    * Load Recolhimento styles after listening mode

--- main.tsx
+++ main.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from "react-dom/client";
 import Home from "./app/page";
+import { RecollectionMode } from "./components/recollection-mode";
 import "./app/globals.css";
 import "./app/repository-library.css";
 import "./app/select-controls.css";
 import "./app/listening-mode.css";
+import "./components/recollection-mode.css";
 
-createRoot(document.getElementById("root")!).render(<Home />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Home />
+    <RecollectionMode />
+  </>,
+);
