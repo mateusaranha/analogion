0b0bd2b63d70ea43f589b1d1808cf8cd6e46f8f5
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-11 15:24:45 -0300

    Load curated library styles

--- main.tsx
+++ main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Home from "./app/page";
 import "./app/globals.css";
+import "./app/repository-library.css";
 
 createRoot(document.getElementById("root")!).render(<Home />);
